4089824eebb08e43f09a7f3c2a789c6877490e34
paul <rohanpaul2@gmail.com>
2020-10-08 17:39:37 +0530

    adding tricky questions

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ A collection of super-popular and Tricky Interview questions, along with explana
 - [closure-MOST-POPULAR-Interview Question on setTimeout](closure-setTimeout-MOST-POPULAR.js)
 - [closures-retains-values-of-outer-function-after-outer-returns](closures-retains-values-of-outer-function-after-outer-returns.md)
 - [closure-tricky-GREAT-EXAMPLE](closure-tricky-GREAT-EXAMPLE.js)
+- [closure-use-case-create-private-variable](closure-use-case-create-private-variable.js)
---
+++ closure-use-case-create-private-variable.js
@@ -0,0 +1,28 @@
+/*
+How would you use a closure to create a private counter?
+ */
+
+const counter = () => {
+  // this is the variable that I have to change
+  let _counter = 0
+  return {
+    add: (increment) => {
+      return (_counter += increment)
+    },
+    retrieve: () => {
+      return "The counter currently is at " + _counter
+    },
+  }
+}
+
+const c = counter()
+console.log(c.add(5))
+c.add(9)
+console.log(c.retrieve())
+
+/*
+Output :
+
+5
+The counter currently is at 14
+ */
